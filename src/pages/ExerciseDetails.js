7d17afd760ec034089d9c90eb74124e639fa89c5
CG-2407 <62751408+CG-2407@users.noreply.github.com>
2022-06-16 13:55:49 -0500

    Horizontal ScrollBar and Exercise Details

--- src/pages/ExerciseDetails.js
+++ src/pages/ExerciseDetails.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import SimilarExercises from '../Components/SimilarExercises';
+import Detail from '../Components/Detail';
+import ExerciseVideo from '../Components/ExerciseVideo';
 
 const ExerciseDetails = () => {
+
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([])
+  const {id} = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      setExerciseDetail(exerciseDetailData);
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetail.name}`,youtubeOptions);
+      setExerciseVideos(exerciseVideosData);
+    }
+    fetchExercisesData();
+  }, [id])
+  
+  
   return (
-    <div>ExerciseDetails</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideo/>
+      <SimilarExercises/>
+    </Box>
   )
 }
 
