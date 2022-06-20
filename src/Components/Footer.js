bf4b4e046dfae46c03f6fa11fb13aafa78bf2ee9
CG-2407 <62751408+CG-2407@users.noreply.github.com>
2022-06-20 10:48:34 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px"> 
+        <img src={Logo} alt="logo" width="200px" height="40px"/>
+        <Typography variant="h5" pb="40px" mt="20px"> Done by CG-2407 </Typography>
+      </Stack>
+    </Box>
   )
 }
 
